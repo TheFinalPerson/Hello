ecc094f95d2d3d63b81abfc3495e6b1a80c71017
TheFinalPerson <bsouthworth619@gmail.com>
2024-11-22 11:34:37 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,12 +1,12 @@
 addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+    name: "power", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#FFD700",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
@@ -26,3 +26,10 @@ addLayer("p", {
     ],
     layerShown(){return true}
 })
+upgrades: {
+11: {
+title: "Little more power please",
+description: "Double your point gain",
+cost: new Decimal(1),
+},	
+},
